3301d0ea6a3fb1bebaa6be7180ccfd2f88fd273c
Siesasaar <sara.leenards@hva.nl>
2022-11-03 16:48:26 +0100

    Updaten voor hulp Nina

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 // JavaScript Document
 console.log("hi");
 
+var eersteImagesButton =
+document.querySelector("section.images li:nth-of-type(1 button");
+
+eersteImagesButton.addEventListener("click", eersteImagesButtonKlik);
+
+function eersteImagesButtonKlik() {
+    eersteImagesButton.classList.toggle("menuOpen");
+}
