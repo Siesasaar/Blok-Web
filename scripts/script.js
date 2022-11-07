4e9746abfe7440fc2cc934f6ff2449b8595ee219
Siesasaar <sara.leenards@hva.nl>
2022-11-07 17:43:49 +0100

    Hover, focus en hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,22 @@
 // JavaScript Document
 console.log("hi");
 
-var openButton =
-document.querySelector("header > button");
+var openButton = document.querySelector("header > button");
+var sluitButton = document.querySelector("nav button");
+var nav = document.querySelector("nav");
 
+
+// menu open
 openButton.addEventListener("click", openMenu);
 
-function openMenu() {
+function openMenu(){
+    nav.classList.add("openMenu");
+}
 
-var deNav =
-document.querySelector("nav");
 
-deNav.classList.add("toonMenu")
+// menu sluit
+sluitButton.addEventListener("click", sluitMenu);
+
+function sluitMenu() {
+    nav.classList.remove("openMenu")
 }
